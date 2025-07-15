62ea77a2ec2c1848455b8b00d27e7e11590cb618
Ruchita Ram <ruchitaram@RuchitaRam-01-LT.local>
2025-07-15 19:25:32 +0530

    added the repository class logic

--- src/main/java/org/example/repository/JackpotRepository.java
+++ src/main/java/org/example/repository/JackpotRepository.java
@@ -1,4 +1,26 @@
 package org.example.repository;
 
+import org.example.model.Jackpot;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
 public class JackpotRepository {
+
+    private final Map<String, Jackpot> jackpots = new ConcurrentHashMap<>();
+
+    public Jackpot getJackpot(String id) {
+        return jackpots.get(id);
+    }
+
+    public void saveJackpot(Jackpot jackpot) {
+        jackpots.put(jackpot.getJackpotId(), jackpot);
+    }
+
+    public Collection<Jackpot> getAllJackpots() {
+        return jackpots.values();
+    }
 }
